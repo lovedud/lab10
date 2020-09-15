d1afce2c709e745d507efef55d3c57e006297862
unale <unaletvt@gmail.com>
2020-09-15 14:45:56 +0300

    lab2 task2

---
+++ lab2_2/Task2/Task2.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace lab2
+{
+    partial class Task2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.filePathLabel = new System.Windows.Forms.Label();
+            this.chooseFileButton = new System.Windows.Forms.Button();
+            this.sourcePictureBox = new System.Windows.Forms.PictureBox();
+            this.colorComboBox = new System.Windows.Forms.ComboBox();
+            this.resultPictureBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.barChartPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.sourcePictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resultPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barChartPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // filePathLabel
+            // 
+            this.filePathLabel.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.filePathLabel.Location = new System.Drawing.Point(197, 22);
+            this.filePathLabel.Name = "filePathLabel";
+            this.filePathLabel.Size = new System.Drawing.Size(659, 23);
+            this.filePathLabel.TabIndex = 0;
+            // 
+            // chooseFileButton
+            // 
+            this.chooseFileButton.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.chooseFileButton.Location = new System.Drawing.Point(12, 12);
+            this.chooseFileButton.Name = "chooseFileButton";
+            this.chooseFileButton.Size = new System.Drawing.Size(168, 39);
+            this.chooseFileButton.TabIndex = 1;
+            this.chooseFileButton.Text = "Выберите файл";
+            this.chooseFileButton.UseVisualStyleBackColor = true;
+            this.chooseFileButton.Click += new System.EventHandler(this.ChooseFileButton_Click);
+            // 
+            // sourcePictureBox
+            // 
+            this.sourcePictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.sourcePictureBox.Location = new System.Drawing.Point(12, 57);
+            this.sourcePictureBox.Name = "sourcePictureBox";
+            this.sourcePictureBox.Size = new System.Drawing.Size(468, 373);
+            this.sourcePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.sourcePictureBox.TabIndex = 2;
+            this.sourcePictureBox.TabStop = false;
+            // 
+            // colorComboBox
+            // 
+            this.colorComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.colorComboBox.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.colorComboBox.FormattingEnabled = true;
+            this.colorComboBox.Items.AddRange(new object[] {
+            "",
+            "R",
+            "G",
+            "B"});
+            this.colorComboBox.Location = new System.Drawing.Point(876, 19);
+            this.colorComboBox.Name = "colorComboBox";
+            this.colorComboBox.Size = new System.Drawing.Size(86, 27);
+            this.colorComboBox.TabIndex = 3;
+            this.colorComboBox.SelectedIndexChanged += new System.EventHandler(this.ColorComboBox_SelectedIndexChanged);
+            // 
+            // resultPictureBox
+            // 
+            this.resultPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.resultPictureBox.Location = new System.Drawing.Point(494, 57);
+            this.resultPictureBox.Name = "resultPictureBox";
+            this.resultPictureBox.Size = new System.Drawing.Size(468, 373);
+            this.resultPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.resultPictureBox.TabIndex = 4;
+            this.resultPictureBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(441, 444);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Гистограмма";
+            // 
+            // barChartPictureBox
+            // 
+            this.barChartPictureBox.Location = new System.Drawing.Point(232, 471);
+            this.barChartPictureBox.Name = "barChartPictureBox";
+            this.barChartPictureBox.Size = new System.Drawing.Size(512, 200);
+            this.barChartPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.barChartPictureBox.TabIndex = 6;
+            this.barChartPictureBox.TabStop = false;
+            // 
+            // Task2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(986, 689);
+            this.Controls.Add(this.barChartPictureBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.resultPictureBox);
+            this.Controls.Add(this.colorComboBox);
+            this.Controls.Add(this.sourcePictureBox);
+            this.Controls.Add(this.chooseFileButton);
+            this.Controls.Add(this.filePathLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Task2";
+            this.Text = "Task2";
+            ((System.ComponentModel.ISupportInitialize)(this.sourcePictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resultPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barChartPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Label filePathLabel;
+        private System.Windows.Forms.Button chooseFileButton;
+        public System.Windows.Forms.PictureBox sourcePictureBox;
+        public System.Windows.Forms.ComboBox colorComboBox;
+        public System.Windows.Forms.PictureBox resultPictureBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox barChartPictureBox;
+    }
+}
---
+++ lab2_2/Task2/Task2.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab2
+{
+    public partial class Task2 : Form
+    {
+        string selectedFileName;
+        List<Image> colorImages;
+        List<Image> barCharts;
+
+        void GetColorImagesAndBarCharts(Image source, out List<Image> colorImages, out List<Image> barCharts)
+        {
+            Bitmap sourceBitmap = new Bitmap(source);
+
+            Bitmap redBitmap = new Bitmap(sourceBitmap.Width, sourceBitmap.Height);
+            Bitmap greenBitmap = new Bitmap(sourceBitmap.Width, sourceBitmap.Height);
+            Bitmap blueBitmap = new Bitmap(sourceBitmap.Width, sourceBitmap.Height);
+
+            int[] redColors = new int[256];
+            int[] greenColors = new int[256];
+            int[] blueColors = new int[256];
+
+            for (int i = 0; i < 256; i++)
+            {
+                redColors[i] = 0;
+                greenColors[i] = 0;
+                blueColors[i] = 0;
+            }
+
+            for (int x = 0; x < sourceBitmap.Width; x++)
+            {
+                for (int y = 0; y < sourceBitmap.Height; y++)
+                {
+                    Color color = sourceBitmap.GetPixel(x, y);
+
+                    //данные для гистограммы
+                    redColors[color.R]++;
+                    greenColors[color.G]++;
+                    blueColors[color.B]++;
+
+                    //пиксели результирующих картинок R,G,B
+                    redBitmap.SetPixel(x, y, Color.FromArgb(color.R, 0, 0));
+                    greenBitmap.SetPixel(x, y, Color.FromArgb(0, color.G, 0));
+                    blueBitmap.SetPixel(x, y, Color.FromArgb(0, 0, color.B));
+                }
+            }
+            
+            sourceBitmap.Dispose();
+
+            colorImages = new List<Image> { redBitmap, greenBitmap, blueBitmap };
+
+            int maxR = redColors.Max();
+            int maxG = greenColors.Max();
+            int maxB = blueColors.Max();
+
+            Bitmap redBarChart = new Bitmap(512, 200);
+            Bitmap greenBarChart = new Bitmap(512, 200);
+            Bitmap blueBarChart = new Bitmap(512, 200);
+
+            Graphics graphicsR = Graphics.FromImage(redBarChart);
+            Graphics graphicsG = Graphics.FromImage(greenBarChart);
+            Graphics graphicsB = Graphics.FromImage(blueBarChart);
+
+            Pen penR = new Pen(Color.Red, 2);
+            Pen penG = new Pen(Color.Green, 2);
+            Pen penB = new Pen(Color.Blue, 2);
+
+            for (int x = 0; x < 512; x += 2)
+            {
+                int heightR = (int)(200.0 * redColors[x / 2] / maxR);
+                int heightG = (int)(200.0 * greenColors[x / 2] / maxG);
+                int heightB = (int)(200.0 * blueColors[x / 2] / maxB);
+
+                graphicsR.DrawRectangle(penR, x, 200 - heightR, 2, heightR);
+                graphicsG.DrawRectangle(penG, x, 200 - heightG, 2, heightG);
+                graphicsB.DrawRectangle(penB, x, 200 - heightB, 2, heightB);
+            }
+
+            //освобождаем ресурсы
+            penR.Dispose();
+            penG.Dispose();
+            penB.Dispose();
+            graphicsR.Dispose();
+            graphicsG.Dispose();
+            graphicsB.Dispose();
+
+            barCharts = new List<Image> { redBarChart, greenBarChart, blueBarChart };
+        }
+
+        void ShowSourceImage()
+        {
+            if (selectedFileName != null)
+            {
+                Image sourceImage = Image.FromFile(selectedFileName);
+                sourcePictureBox.Image = sourceImage;
+                resultPictureBox.Image = null;
+                barChartPictureBox.Image = null;
+                //сохраняем в памяти все цветные изображения и гистограммы
+                GetColorImagesAndBarCharts(sourceImage, out colorImages, out barCharts); 
+            }
+            else
+            {
+                sourcePictureBox.Image = null;
+                resultPictureBox.Image = null;
+                barChartPictureBox.Image = null;
+                colorImages = null;
+                barCharts = null;
+            }
+        }
+
+        public Task2()
+        {
+            InitializeComponent();
+        }
+
+        private void ChooseFileButton_Click(object sender, EventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Multiselect = false;
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                selectedFileName = dialog.FileName;
+                filePathLabel.Text = selectedFileName;
+                colorComboBox.SelectedIndex = 0;
+            }
+            else
+            {
+                selectedFileName = null;
+            }
+            dialog.Dispose();
+            ShowSourceImage();
+        }
+
+        private void ColorComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (colorImages == null) return;
+            if (colorComboBox.SelectedIndex == 0)
+            {
+                resultPictureBox.Image = null;
+                barChartPictureBox.Image = null;
+            }
+            else
+            {
+                resultPictureBox.Image = colorImages[colorComboBox.SelectedIndex - 1];
+                barChartPictureBox.Image = barCharts[colorComboBox.SelectedIndex - 1];
+            }
+        }
+    }
+}
